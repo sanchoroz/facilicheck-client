a0950526aa2f4a2646b717e30bb2c4c96eba2342
AlexR <rozenbergalx@gmail.com>
2023-04-30 15:09:21 +0200

    config new

--- cypress/e2e/app-test.cy.js
+++ cypress/e2e/app-test.cy.js
@@ -4,7 +4,7 @@ describe('Login page title', () => {
   });
 
   it('should be correct', () => {
-    cy.get('[data-cy=login-title]').contains('התחברות');
+    cy.get('[data-cy=login-title]').contains('Login');
     cy.get('[data-cy=name-input]').type('serverstage@admin.com');
     cy.get('[data-cy=password-input]').type('755205');
     cy.get('[data-cy=login-button]').click();
