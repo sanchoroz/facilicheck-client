7061795c932eb963bb80cb8de5cc0db3c3c49bcf
alexanderr <alexanderr@wix.com>
2023-05-26 16:40:07 +0200

    test modified2

--- cypress/e2e/login/logout.cy.js
+++ cypress/e2e/login/logout.cy.js
@@ -7,10 +7,11 @@ describe("Logout", () => {
   });
 
   it("should redirect to login page", () => {
-    
+
     LoginPage.doLogin("stage@admin.com", "755205");
     cy.get("[data-cy=home-title]").should("contain", "Home");
     Header.doLogout();
     LoginPage.isLoaded();
+    
   });
 });
