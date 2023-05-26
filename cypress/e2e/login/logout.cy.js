adbd57265233b22b824118caa74e8046579e457d
alexanderr <alexanderr@wix.com>
2023-05-26 15:21:41 +0200

    logout modified

---
+++ cypress/e2e/login/logout.cy.js
@@ -0,0 +1,15 @@
+import Header from "../../page-objects/components/Header";
+import LoginPage from "../../page-objects/pages/LoginPage";
+
+describe("Logout", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should redirect to login page", () => {
+    LoginPage.doLogin("stage@admin.com", "755205");
+    cy.get("[data-cy=home-title]").should("contain", "Home");
+    Header.doLogout();
+    LoginPage.isLoaded();
+  });
+});
